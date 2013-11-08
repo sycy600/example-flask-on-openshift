4277d1ffb5f1ce7277b432d9f9a3df9eb399d9ee
sycy600 <sycy600@gmail.com>
2013-11-08 20:30:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.1.0",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=["setuptools", "flask==0.10.0"],
+    install_requires=["setuptools", "flask==0.10"],
 )
